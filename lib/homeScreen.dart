51563ca8e255546483cdd1567588f05a80ddf23b
albin2771git <albinthomas2771@gmail.com>
2022-11-22 09:10:00 +0530

    first commit

---
+++ lib/homeScreen.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class HomeScreen extends StatefulWidget {
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  bool flag = true;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Center(
+            child: AnimatedSwitcher(
+                duration: Duration(
+                  milliseconds: 3000,
+                ),
+                reverseDuration: Duration(milliseconds: 1000),
+                transitionBuilder: (Widget child, Animation<double> animation) {
+                  return ScaleTransition(scale: animation, child: child);
+                },
+                child: flag
+                    ? Container(
+                        width: 100,
+                        height: 100,
+                        color: Colors.green,
+                        child: Center(child: Text("Container 1")),
+                        key: Key('1'))
+                    : Container(
+                        width: 300,
+                        height: 100,
+                        color: Colors.red,
+                        child: Center(child: Text("Container 2")),
+                        key: Key('2'))),
+          ),
+          SizedBox(
+            height: 20,
+          ),
+          ElevatedButton(
+              onPressed: () {
+                setState(() {
+                  flag = !flag;
+                });
+              },
+              child: Text('next'))
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,9 @@
+import 'package:animatedswitcher/homeScreen.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: HomeScreen(),
+  ));
+}
